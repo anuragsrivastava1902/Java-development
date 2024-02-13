dd6da83d83b8f1626f81b701a3ecfe3971057490
anuragsrivastava1902 <anurag.srivastava1902@gmail.com>
2024-02-13 23:17:01 +0530

    Updated MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,7 +1,3 @@
-//REQ: Writing my first java program
-
-package com.excelr.learningjava.dayone;
-
 
 class MyFirstJavaProgram{
 	
@@ -11,27 +7,8 @@ class MyFirstJavaProgram{
 	
 }
 
-/*
-please ensure you are iin your project src folder 
-
-compilation command: (.class gets generated)
-----------------------
-javac -d ..\classes com\excelr\learningjava\dayone\MyFirstJavaProgram.java
-
-note:- by mistake if we don't write -d ..\classes then java compiler won't create package/folder in the ..\classes location.
-
-Exceution Command: (take that .class and execute by giving classname)
------------------------
-syntax:- java -classpath ..\classes pacakgename.classname
-
-java -cp ..\classes com.excelr.learningjava.dayone.MyFirstJavaProgram
-
-Analyse Command:
--------------------
-javap -cp ..\classes com.excelr.learningjava.dayone.MyFirstJavaProgram
 
 
 
 
 
-*/
